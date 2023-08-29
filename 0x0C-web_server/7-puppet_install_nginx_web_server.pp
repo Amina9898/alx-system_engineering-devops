bf2bbd578bc402cab1ce03460040883fff3089ac
Frida <amina.amer.9898@gmail.com>
2023-08-29 13:01:25 -0700

    Task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,30 @@
+class nginx_redirect {
+  package { 'nginx':
+    ensure => 'installed',
+  }
+
+  service { 'nginx':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/html/index.html':
+    content => "Hello, World!\n",
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    content => "server {
+      listen 80;
+      server_name _;
+      root /var/www/html;
+
+      location / {
+        return 301 http://www.example.com/redirected-page;
+      }
+    }",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
+}
