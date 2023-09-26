69575486a1237d56c25b111e2b086127916e4d8c
Frida <amina.amer.9898@gmail.com>
2023-09-25 17:29:38 -0700

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This is a script that fetches employeie TODO list
+informaation from an API
+"""
+
+
+import sys
+import requests
+
+
+def fetch_employee_todo(employee_id):
+    """ function that fetches the required todo info """
+    URL = "https://jsonplaceholder.typicode.com"
+
+    user_info = requests.get(f"{URL}/users/{employee_id}")
+    user_info = user_info.json()
+    emp_name = user_info.get('name')
+
+    todo = requests.get(f"{URL}/todos?userId={employee_id}")
+    todo = todo.json()
+
+    total = len(todo)
+    completed = sum(1 for task in todo if task['completed'])
+
+    print(f"Employee {emp_name} is done with tasks ({completed}/{total}):")
+
+    for task in todo:
+        if task['completed']:
+            print("\t{}".format(task['title']))
+
+
+if __name__ == "__main__":
+
+    employee_id = int(sys.argv[1])
+    fetch_employee_todo(employee_id)
