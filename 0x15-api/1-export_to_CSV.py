8066fc07447c3b7c526d54d392511f8c2487879d
Frida <amina.amer.9898@gmail.com>
2023-09-25 19:27:26 -0700

    Task2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,19 +10,19 @@ import requests
 import sys
 
 
-def export_to_csv(employee_id):
+def export_to_csv(user_id):
     """ function that fetches the required todo info """
 
     URL = "https://jsonplaceholder.typicode.com"
 
-    user_info = requests.get(f"{URL}/users/{employee_id}")
+    user_info = requests.get(f"{URL}/users/{user_id}")
     user_info = user_info.json()
-    emp_name = user_info.get('name')
+    username = user_info.get('username')
 
-    todo = requests.get(f"{URL}/todos?userId={employee_id}")
+    todo = requests.get(f"{URL}/todos?userId={user_id}")
     todo = todo.json()
 
-    filename = f"{employee_id}.csv"
+    filename = f"{user_id}.csv"
 
     with open(filename, mode="w", newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
@@ -32,10 +32,10 @@ def export_to_csv(employee_id):
                 status = True
             else:
                 status = False
-            writer.writerow([employee_id, emp_name, status, task['title']])
+            writer.writerow([user_id, username, status, task['title']])
 
 
 if __name__ == "__main__":
 
-    employee_id = int(sys.argv[1])
-    export_to_csv(employee_id)
+    user_id = int(sys.argv[1])
+    export_to_csv(user_id)
