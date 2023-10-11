07ffd0d8a61217c930f02a604a28ba9ede1572c2
Amina Amer <amina.amer.9898@gmail.com>
2023-10-10 19:41:20 -0700

    fix url request

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,12 +12,15 @@ def number_of_subscribers(subreddit):
     number of subscribers for a given subreddit.
     If an invalid subreddit is given, the function should return 0
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "app"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.exceptions.RequestException:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
